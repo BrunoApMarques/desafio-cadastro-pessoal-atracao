e6587eac7b5269c506b46c7a9c2d8f183943170f
Bruno Marques <brunoa879@gmail.com>
2021-08-29 01:43:24 -0300

    Adicionado nova imagem e alterações nas pastas README.md,index.html,script.js e style.css

---
+++ README.md
@@ -0,0 +1,36 @@
+# Desafio Individual - Formulário Pessoal
+
+## Autor:
+
+Bruno Aparecido Marques
+
+## Idade:
+
+32 anos
+
+## Objetivo:
+
+Desenvolver um site de cadastro pessoal.
+
+## Ferramenta:
+
+Visual Studio Code no Windows.
+
+## Linguagens:
+
+CSS
+
+HTML
+
+JavaScript
+
+## Referências:
+
+W3schools.com
+
+Canal Hora de Codar
+
+TreinaWeb
+
+
+
--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-//Funçao  Validação//
+// Funçao  Validação
 
-function _cpf(cpf) {
-    cpf = cpf.replace(/[^\d]+/g,'');
+function _cpf(cpf) 
+{
+    cpf = cpf.replace(/[^\d]+/g, '');
 
     if (cpf == '') return false;
-    if (cpf.length !=11 ||
+
+    if (cpf.length != 11 ||
         cpf == "00000000000" ||
         cpf == "11111111111" ||
         cpf == "22222222222" ||
@@ -18,49 +20,64 @@ function _cpf(cpf) {
 
         return false;
 
-        add = 0;
-        for(i = 0; i < 9; i++)
+    add = 0;
+
+    for (i = 0; i < 9; i++)
         add += parseInt(cpf.charAt(i)) * (10 - i);
-        rev = 11 - (add % 11);
-        if(rev == 10 || rev == 11)
+
+    rev = 11 - (add % 11);
+
+    if (rev == 10 || rev == 11)
         rev = 0;
-        if(rev != parseInt(cpf.charAt(9)))
+
+    if (rev != parseInt(cpf.charAt(9)))
         return false;
-        add = 0;
-        for (i = 0; i < 10; i++)
-        add +=parseInt(cpf.charAt(i)) * (11 - i);
-        rev = 11 - (add % 11);
-        if(rev == 10 || rev == 11)
+
+    add = 0;
+
+    for (i = 0; i < 10; i++)
+        add += parseInt(cpf.charAt(i)) * (11 - i);
+
+    rev = 11 - (add % 11);
+
+    if (rev == 10 || rev == 11)
         rev = 0;
-        if(rev != parseInt(cpf.charAt(10)))
+
+    if (rev != parseInt(cpf.charAt(10)))
         return false;
-        return true;
+
+    return true;
 }
 
-// Apresenta que CPF não é valido, alerta a mensagem, em seguida e limpa o valor //
-function validarCPF(cpf){
-    if( !_cpf(cpf.value)){
+// Apresenta que CPF não é valido, alerta a mensagem, em seguida e limpa o valor
+function validarCPF(cpf) 
+{
+    if (!_cpf(cpf.value)) 
+    {
         alert("CPF inválido!");
         cpf.value = "";
     }
 }
 
-//Botão Cadastrar,recebe mensagem diz que foi cadastrado e mostra a página do site cadastrado com sucesso// 
-function cadastrar() {
-    location.href="envio.html"
-    var nome = document.getElementById("nome");
+// Botão Cadastrar, recebe mensagem diz que foi cadastrado e mostra a página do site cadastrado com sucesso
+function cadastrar() 
+{
+    window.location.href = "envio.html";
+    const nome = document.getElementById("nome");
 
-    if (nome.value != "") {
+    if (nome.value != "") 
+    {
         alert(nome.value + ',' + 'cadastrado!');
     }
 }
 
 
-// *//
+// *
 const cadastro = document.getElementById('formulario')
 
-cadastro.addEventListener('submit', function(e){
-    e.preventDefault()
+cadastro.addEventListener('submit', function (e) 
+{
+    e.preventDefault();
     let nome = document.getElementById('nome').value;
     let cpf = document.getElementById('cpf').value;
     let rg = document.getElementById('rg').value;
@@ -74,11 +91,15 @@ cadastro.addEventListener('submit', function(e){
     let telefonefixo = document.getElementById('telefonefixo').value;
     let telefonecelular = document.getElementById('telefonecelular').value;
 
- }).then( response =>{
-        if (response.status === 200) {
-            alert('Cadastrado com sucesso');
-        }else {
-            alert("ERROR")   
-        }
-    })
+}).then(response => 
+{
+    if (response.status === 200) 
+    {
+        alert('Cadastrado com sucesso');
+    } 
     
+    else 
+    {
+        alert("FALHA");
+    }
+})
